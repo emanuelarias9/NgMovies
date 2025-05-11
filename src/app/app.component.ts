74a7dc25bb99916aa845d851f6f9fb1685965959
Emanuel Arias <Emanuelarias9@gmail.com>
2025-05-10 19:08:28 -0500

    Created ListadoPeliculasComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ListadoPeliculasComponent } from './peliculas/listado-peliculas/listado-peliculas.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ListadoPeliculasComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'NgMovies';
-}
+export class AppComponent {}
