7ff08cc3eafcfcba28e8f67d5714054283a7ef2d
Emanuel Arias <Emanuelarias9@gmail.com>
2025-05-15 20:01:15 -0500

    Add EditarActorComponent with HTML, CSS, and TypeScript files

---
+++ src/app/actores/editar-actor/editar-actor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-actor',
+  imports: [],
+  templateUrl: './editar-actor.component.html',
+  styleUrl: './editar-actor.component.css'
+})
+export class EditarActorComponent {
+
+}
