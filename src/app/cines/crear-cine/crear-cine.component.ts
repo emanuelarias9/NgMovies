022dd3bc40d330ca9458031bb60b0624062c2ab8
Emanuel Arias <Emanuelarias9@gmail.com>
2025-05-19 18:01:10 -0500

    Add CrearCine and EditarCine components with form handling and validation

--- src/app/cines/crear-cine/crear-cine.component.ts
+++ src/app/cines/crear-cine/crear-cine.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CrearCineDTO } from '../cine';
+import { FormularioCinesComponent } from '../formulario-cines/formulario-cines.component';
 
 @Component({
   selector: 'app-crear-cine',
-  imports: [],
+  imports: [FormularioCinesComponent],
   templateUrl: './crear-cine.component.html',
-  styleUrl: './crear-cine.component.css'
+  styleUrl: './crear-cine.component.css',
 })
 export class CrearCineComponent {
-
+  guardarCambios(cine: CrearCineDTO) {
+    // this.router.navigate(['/generos']);
+    console.log('creando el cine: ', cine);
+  }
 }
--- src/app/cines/editar-cine/editar-cine.component.ts
+++ src/app/cines/editar-cine/editar-cine.component.ts
@@ -1,12 +1,20 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { CineDTO, CrearCineDTO } from '../cine';
+import { FormularioCinesComponent } from '../formulario-cines/formulario-cines.component';
 
 @Component({
   selector: 'app-editar-cine',
-  imports: [],
+  imports: [FormularioCinesComponent],
   templateUrl: './editar-cine.component.html',
   styleUrl: './editar-cine.component.css',
 })
 export class EditarCineComponent {
   @Input({ transform: numberAttribute })
   id!: number;
+
+  cine: CineDTO = { id: 1, nombre: 'royal films' };
+
+  guardarCambios(cine: CrearCineDTO) {
+    console.log('editando el cine: ', cine);
+  }
 }
