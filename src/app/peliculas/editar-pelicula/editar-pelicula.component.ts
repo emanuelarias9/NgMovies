aeaa86da9be7b9e175359af0556987e366d562ee
Emanuel Arias <Emanuelarias9@gmail.com>
2025-05-21 22:42:20 -0500

    Refactor EditarPelicula component to include form handling and update template structure

--- src/app/peliculas/editar-pelicula/editar-pelicula.component.ts
+++ src/app/peliculas/editar-pelicula/editar-pelicula.component.ts
@@ -1,12 +1,26 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { CrearPeliculaDTO, PeliculaDTO } from '../pelicula';
+import { FormularioPeliculasComponent } from '../formulario-peliculas/formulario-peliculas.component';
 
 @Component({
   selector: 'app-editar-pelicula',
-  imports: [],
+  imports: [FormularioPeliculasComponent],
   templateUrl: './editar-pelicula.component.html',
   styleUrl: './editar-pelicula.component.css',
 })
 export class EditarPeliculaComponent {
   @Input({ transform: numberAttribute })
   id!: number;
+
+  pelicula: PeliculaDTO = {
+    id: 1,
+    titulo: 'alexa',
+    trailer: ':v',
+    fechaLanzamiento: new Date(1991, 0, 25),
+    poster:
+      'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832',
+  };
+  guardarCambios(pelicula: CrearPeliculaDTO) {
+    console.log('Editando la pelicula: ', pelicula);
+  }
 }
