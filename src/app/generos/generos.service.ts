30e0be2efeb844232a6a0365ac3f2b8e19346f07
Emanuel Arias <Emanuelarias9@gmail.com>
2025-06-02 19:38:39 -0500

    Add environment configurations for development and production, and update GenerosService to use the environment variables for API requests

--- src/app/generos/generos.service.ts
+++ src/app/generos/generos.service.ts
@@ -2,16 +2,18 @@ import { inject, Injectable } from '@angular/core';
 import { GeneroDTO } from './genero';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GenerosService {
-  private http = inject(HttpClient);
-
   constructor() {}
 
+  private http = inject(HttpClient);
+  private urlPeticion = environment.urlApiBase + '/Generos';
+
   public ListarGenero(): Observable<GeneroDTO[]> {
-    return this.http.get<GeneroDTO[]>('https://localhost:7066/api/Generos');
+    return this.http.get<GeneroDTO[]>(this.urlPeticion);
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  urlApiBase: 'https://localhost:7066/api',
+};
