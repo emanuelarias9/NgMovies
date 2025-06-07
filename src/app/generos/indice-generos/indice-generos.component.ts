b30e1f6d92a624eaa6bc01e0a226b34fe00ea6d9
Emanuel Arias <Emanuelarias9@gmail.com>
2025-06-06 21:38:11 -0500

    Enhance IndiceGenerosComponent to display genre data in a table format with edit and delete actions

--- src/app/generos/indice-generos/indice-generos.component.ts
+++ src/app/generos/indice-generos/indice-generos.component.ts
@@ -2,19 +2,29 @@ import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { GenerosService } from '../generos.service';
+import { GeneroDTO } from '../genero';
+import { ListadoGenericoComponent } from '../../compartidos/componentes/listado-generico/listado-generico.component';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-indice-generos',
-  imports: [RouterLink, MatButtonModule],
+  imports: [
+    RouterLink,
+    MatButtonModule,
+    ListadoGenericoComponent,
+    MatTableModule,
+  ],
   templateUrl: './indice-generos.component.html',
   styleUrl: './indice-generos.component.css',
 })
 export class IndiceGenerosComponent {
   generoServive = inject(GenerosService);
-
+  generos!: GeneroDTO[];
+  columns = ['Id', 'Nombre', 'Acciones'];
   constructor() {
     this.generoServive.ListarGenero().subscribe((generos) => {
-      console.log(generos);
+      this.generos = generos;
+      console.log(this.generos);
     });
   }
 }
