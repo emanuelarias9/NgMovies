e207318592874cf1c788031f1c524dea4f00fd94
Emanuel Arias <Emanuelarias9@gmail.com>
2025-06-20 17:51:07 -0500

    Refactor CrearActorComponent to utilize CrearEntidadComponent for actor creation, removing unnecessary error handling and form logic.

--- src/app/actores/crear-actor/crear-actor.component.ts
+++ src/app/actores/crear-actor/crear-actor.component.ts
@@ -1,31 +1,16 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormularioActoresComponent } from '../formulario-actores/formulario-actores.component';
-import { CrearActorDTO } from '../actores';
 import { ActoresService } from '../actores.service';
-import { Router } from '@angular/router';
-import { errorHandler } from '../../compartidos/funciones/errorHandler';
-import { ErrorHandlerComponent } from '../../compartidos/errores/error-handler/error-handler.component';
+import { CrearEntidadComponent } from '../../compartidos/componentes/crear-entidad/crear-entidad.component';
+import { ServicioCrudToken } from '../../compartidos/proveedores/proveedores';
 
 @Component({
   selector: 'app-crear-actor',
-  imports: [FormularioActoresComponent, ErrorHandlerComponent],
+  imports: [CrearEntidadComponent],
   templateUrl: './crear-actor.component.html',
   styleUrl: './crear-actor.component.css',
+  providers: [{ provide: ServicioCrudToken, useClass: ActoresService }],
 })
 export class CrearActorComponent {
-  actoresService = inject(ActoresService);
-  router = inject(Router);
-  errorMessages: string[] = [];
-
-  guardarCambios(actor: CrearActorDTO) {
-    this.actoresService.Crear(actor).subscribe({
-      next: () => {
-        this.router.navigate(['/actores']);
-      },
-      error: (err) => {
-        const errors = errorHandler(err);
-        this.errorMessages = errors;
-      },
-    });
-  }
+  formularioActores = FormularioActoresComponent;
 }
