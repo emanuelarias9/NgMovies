d58836a6067d5aeb20e43a17f45f4e61fb152791
Emanuel Arias <Emanuelarias9@gmail.com>
2025-06-20 17:51:21 -0500

    Refactor CrearGeneroComponent to use CrearEntidadComponent for genre creation, removing unnecessary error handling and form logic.

--- src/app/generos/crear-genero/crear-genero.component.ts
+++ src/app/generos/crear-genero/crear-genero.component.ts
@@ -1,14 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { Router, RouterLink } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormularioGeneroComponent } from '../formulario-genero/formulario-genero.component';
-import { CrearGeneroDTO } from '../genero';
 import { GenerosService } from '../generos.service';
-import { errorHandler } from '../../compartidos/funciones/errorHandler';
-import { ErrorHandlerComponent } from '../../compartidos/errores/error-handler/error-handler.component';
+import { ServicioCrudToken } from '../../compartidos/proveedores/proveedores';
+import { CrearEntidadComponent } from '../../compartidos/componentes/crear-entidad/crear-entidad.component';
 
 @Component({
   selector: 'app-crear-genero',
@@ -17,25 +15,12 @@ import { ErrorHandlerComponent } from '../../compartidos/errores/error-handler/e
     MatFormFieldModule,
     ReactiveFormsModule,
     MatInputModule,
-    FormularioGeneroComponent,
-    ErrorHandlerComponent,
+    CrearEntidadComponent,
   ],
   templateUrl: './crear-genero.component.html',
   styleUrl: './crear-genero.component.css',
+  providers: [{ provide: ServicioCrudToken, useClass: GenerosService }],
 })
 export class CrearGeneroComponent {
-  router = inject(Router);
-  private generoService = inject(GenerosService);
-  errorMessages: string[] = [];
-  guardarCambios(genero: CrearGeneroDTO) {
-    this.generoService.Crear(genero).subscribe({
-      next: () => {
-        this.router.navigate(['/generos']);
-      },
-      error: (err) => {
-        const errors = errorHandler(err);
-        this.errorMessages = errors;
-      },
-    });
-  }
+  formularioGenero = FormularioGeneroComponent;
 }
