8c75b0e18355636fd694019694b199b0402b3ed8
Emanuel Arias <Emanuelarias9@gmail.com>
2025-06-20 23:02:22 -0500

    Refactor EditarGeneroComponent to use EditarEntidadComponent, simplifying the template and removing unused logic.

--- src/app/generos/editar-genero/editar-genero.component.ts
+++ src/app/generos/editar-genero/editar-genero.component.ts
@@ -1,49 +1,18 @@
-import {
-  Component,
-  inject,
-  Input,
-  numberAttribute,
-  OnInit,
-} from '@angular/core';
+import { Component, Input, numberAttribute } from '@angular/core';
 import { FormularioGeneroComponent } from '../formulario-genero/formulario-genero.component';
-import { CrearGeneroDTO, GeneroDTO } from '../genero';
 import { GenerosService } from '../generos.service';
-import { LoadingComponent } from '../../compartidos/componentes/loading/loading.component';
-import { Router } from '@angular/router';
-import { errorHandler } from '../../compartidos/funciones/errorHandler';
+import { ServicioCrudToken } from '../../compartidos/proveedores/proveedores';
+import { EditarEntidadComponent } from '../../compartidos/componentes/editar-entidad/editar-entidad.component';
 
 @Component({
   selector: 'app-editar-genero',
-  imports: [FormularioGeneroComponent, LoadingComponent],
+  imports: [EditarEntidadComponent],
   templateUrl: './editar-genero.component.html',
   styleUrl: './editar-genero.component.css',
+  providers: [{ provide: ServicioCrudToken, useClass: GenerosService }],
 })
-export class EditarGeneroComponent implements OnInit {
-  ngOnInit(): void {
-    this.generoService.Obtener(this.id).subscribe((genero) => {
-      this.genero = genero;
-    });
-  }
-
-  generoService = inject(GenerosService);
-  router = inject(Router);
-
+export class EditarGeneroComponent {
   @Input({ transform: numberAttribute })
   id!: number;
-
-  errores: string[] = [];
-
-  genero?: GeneroDTO;
-
-  guardarCambios(genero: CrearGeneroDTO) {
-    this.generoService.Actualizar(this.id, genero).subscribe({
-      next: () => {
-        this.router.navigate(['/generos']);
-      },
-      error: (err) => {
-        const errores = errorHandler(err);
-        this.errores = errores;
-      },
-    });
-  }
+  formularioGenero = FormularioGeneroComponent;
 }
