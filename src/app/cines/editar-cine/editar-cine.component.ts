d016f932f6b6be09e36dd127fba4d46a5c51de00
Emanuel Arias <Emanuelarias9@gmail.com>
2025-06-21 19:45:39 -0500

    Refactor EditarCineComponent to use EditarEntidadComponent, simplifying the template and removing unused logic.

--- src/app/cines/editar-cine/editar-cine.component.ts
+++ src/app/cines/editar-cine/editar-cine.component.ts
@@ -1,25 +1,19 @@
 import { Component, Input, numberAttribute } from '@angular/core';
-import { CineDTO, CrearCineDTO } from '../cine';
 import { FormularioCinesComponent } from '../formulario-cines/formulario-cines.component';
+import { ServicioCrudToken } from '../../compartidos/proveedores/proveedores';
+import { CinesService } from '../cines.service';
+import { EditarEntidadComponent } from '../../compartidos/componentes/editar-entidad/editar-entidad.component';
 
 @Component({
   selector: 'app-editar-cine',
-  imports: [FormularioCinesComponent],
+  imports: [EditarEntidadComponent],
   templateUrl: './editar-cine.component.html',
   styleUrl: './editar-cine.component.css',
+  providers: [{ provide: ServicioCrudToken, useClass: CinesService }],
 })
 export class EditarCineComponent {
   @Input({ transform: numberAttribute })
   id!: number;
 
-  cine: CineDTO = {
-    id: 1,
-    nombre: 'royal films',
-    latitud: 11.009746102368025,
-    longitud: -74.82012370182068,
-  };
-
-  guardarCambios(cine: CrearCineDTO) {
-    console.log('editando el cine: ', cine);
-  }
+  formularioCines = FormularioCinesComponent;
 }
